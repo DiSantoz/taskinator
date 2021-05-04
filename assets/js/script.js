024494f1fc7788949a77efdd4e16af0610a28462
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-05-04 16:15:00 -0400

    created form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //creates a function that creates a new task, styles the task, adds text, adds this element to task list
-var createTaskHandler = function (){
+var createTaskHandler = function (event){
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 //when button is clicked, it calls up on the function above!
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
